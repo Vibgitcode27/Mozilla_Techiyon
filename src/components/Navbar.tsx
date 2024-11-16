1f973e31a6a482ecd97a81cc6f0a9678083efded
Vibhor <vibgitcode27@gmail.com>
2024-11-16 16:30:32 +0530

    added footer

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,32 +1,57 @@
-import { Flex , Button } from "antd";
-import { Fira_Sans_Condensed , Schibsted_Grotesk } from "next/font/google";
-import { TwitterOutlined, InstagramOutlined, WhatsAppOutlined } from "@ant-design/icons";
+"use client"
 
-const firaSansCondensed =  Fira_Sans_Condensed({
+import { Flex, Button } from "antd";
+import { Fira_Sans_Condensed } from "next/font/google";
+import { TwitterOutlined, InstagramOutlined, WhatsAppOutlined } from "@ant-design/icons";
+import "../styles/navbar.css";
+import { useRouter } from "next/dist/client/components/navigation";
+
+const firaSansCondensed = Fira_Sans_Condensed({
   weight: "700",
   subsets: ["latin"], // Add the subset here
 });
 
-export default function Navbar(){
-    return (
-        <div>
-            <Flex justify="space-between" align="center" style={{ paddingInline : "70px" , paddingTop : "10px"}}>
-                <Flex>
-                    <h1 className={firaSansCondensed.className} style={{ fontWeight : "800" , fontSize : "20px"  }}>WAR.FOR.TREASURE</h1>
-                </Flex>
-                <Flex gap={90} justify="center" align="center">
-                    <Flex justify="center" gap={20} style={{ fontWeight : "600"}}>
-                        <h1>EVENT</h1>
-                        <h1>MANGA</h1>
-                        <h1>DESIGN</h1>
-                        <h1>TRESARUE HUNT</h1>
-                    </Flex>
-                    <Flex gap={10}>
-                        <Button style={{ border : "2px solid black" , fontWeight : "600" , borderRadius : "12px" }}>SIGN UP</Button>
-                        <Button style={{ border : "2px solid black" , backgroundColor : "black"  , color : "white" , borderRadius : "12px"}}>SIGN IN</Button>
-                    </Flex>
-                </Flex>
-            </Flex>
-        </div>
-    );
+export default function Navbar() {
+
+    const router = useRouter();
+
+  return (
+    <div>
+      <Flex
+        justify="space-between"
+        align="center"
+        className="navbar"
+      >
+        <Flex>
+          <h1 className={firaSansCondensed.className} style={{ fontWeight: "800", fontSize: "20px" }}>
+            WAR.FOR.TREASURE
+          </h1>
+        </Flex>
+        <Flex gap={90} justify="center" align="center" className="navbar-links">
+          <Flex justify="center" gap={20} style={{ fontWeight: "600" }} className="navbar-nav-links">
+            <h1 onClick={() => { router.push("https://www.youtube.com/watch?v=j5a0jTc9S10&list=PLrXk_RHmNdhHW95zl63D6IhFn9OW5Ia1N&index=1")}} >EVENT</h1>
+            <h1 onClick={() => { router.push("https://www.youtube.com/watch?v=j5a0jTc9S10&list=PLrXk_RHmNdhHW95zl63D6IhFn9OW5Ia1N&index=1")}} >MANGA</h1>
+            <h1 onClick={() => { router.push("https://www.youtube.com/watch?v=j5a0jTc9S10&list=PLrXk_RHmNdhHW95zl63D6IhFn9OW5Ia1N&index=1")}} >DESIGN</h1>
+            <h1 onClick={() => { router.push("https://www.youtube.com/watch?v=j5a0jTc9S10&list=PLrXk_RHmNdhHW95zl63D6IhFn9OW5Ia1N&index=1")}} >TREASURE HUNT</h1>
+          </Flex>
+          <Flex gap={10} className="navbar-buttons">
+            <Button onClick={() => { router.push("https://www.youtube.com/watch?v=j5a0jTc9S10&list=PLrXk_RHmNdhHW95zl63D6IhFn9OW5Ia1N&index=1")}} style={{ border: "2px solid black", fontWeight: "600", borderRadius: "12px" }}>
+              SIGN UP
+            </Button>
+            <Button
+              onClick={() => { router.push("https://www.youtube.com/watch?v=j5a0jTc9S10&list=PLrXk_RHmNdhHW95zl63D6IhFn9OW5Ia1N&index=1")}}
+              style={{
+                border: "2px solid black",
+                backgroundColor: "black",
+                color: "white",
+                borderRadius: "12px",
+              }}
+            >
+              SIGN IN
+            </Button>
+          </Flex>
+        </Flex>
+      </Flex>
+    </div>
+  );
 }
